c2db1edded7407964c942853f50b759fbb3bd6c1
pierce-smith1 <pierce.smith@rezstream.com>
2024-09-22 15:19:28 -0600

    change sketches to loader pattern

--- src/pages/api/v1/all_sketches.ts
+++ src/pages/api/v1/all_sketches.ts
@@ -1,9 +1,9 @@
 import * as Endpoint from "../../../endpoint";
-import Sketches from "../../../stores/sketch";
+import * as SketchLoader from "../../../stores/sketch";
 import * as SketchTransformers from "../../../transformers/sketch";
 
 export const GET = Endpoint.use_get_handler<"all_sketches">(async (request, override) => {
-    const sketches = Sketches.get();
+    const sketches = await SketchLoader.load_all();
     
     const client_sketches = sketches.map(SketchTransformers.to_api_object);
 
--- src/stores/sketch.ts
+++ src/stores/sketch.ts
@@ -1,6 +1,5 @@
 import Config from "../config";
 
-import * as Store from "./_store";
 import * as Filesystem from "../filesystem";
 
 export type Sketch = {
@@ -8,21 +7,16 @@ export type Sketch = {
     name: string,
 };
 
-export class SketchStore extends Store.Store<Sketch> {
-    async load() {
-        const sketch_root_path = `${Config.authorland_root}/sketches`;
+export async function load_all() {
+    const sketch_root_path = `${Config.authorland_root}/sketches`;
 
-        const sketch_files = (await Filesystem.enumerate(sketch_root_path))
-            .filter(item => item.type === "File");
-        
-        const sketches = sketch_files
-            .map(item => item.name.split("."))
-            .map(([prefix, name]) => ({prefix, name, ordinal: parseInt(prefix)}) as const)
-            .toSorted((a, b) => b.ordinal - a.ordinal);
+    const sketch_files = (await Filesystem.enumerate(sketch_root_path))
+        .filter(item => item.type === "File");
+    
+    const sketches = sketch_files
+        .map(item => item.name.split("."))
+        .map(([prefix, name]) => ({prefix, name, ordinal: parseInt(prefix)}) as const)
+        .toSorted((a, b) => b.ordinal - a.ordinal);
 
-        return sketches;
-    }
+    return sketches;
 }
-
-const store = new SketchStore();
-export default store;
