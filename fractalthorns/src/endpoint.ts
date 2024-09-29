d4209c2e4a5ebe930c269817447409a2919850b6
pierce-smith1 <pierce.smith@rezstream.com>
2024-09-29 15:05:46 -0600

    active corporate speedup protocol

--- fractalthorns/src/endpoint.ts
+++ fractalthorns/src/endpoint.ts
@@ -17,8 +17,6 @@ export function use_get_handler<
     Model = Api.GetEndpoints[EndpointName]["response"]
 >(loader: (request: Params, override_response: (new_response: Response) => Model) => Promise<Model>): ({request}: {request: Request}) => Promise<Response> {
     return async ({request}: {request: Request}) => {
-        await new Promise(resolve => setTimeout(resolve, 2000));
-
         const url_params = get_parameters(request);
         const request_params = url_params as Params;
 
