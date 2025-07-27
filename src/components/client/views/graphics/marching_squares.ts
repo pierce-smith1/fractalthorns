18160396450c34a52076aac0091f04ae1c6ef1f8
pierce-smith1 <pierce.smith@rezstream.com>
2025-07-27 01:19:00 -0600

    (client) Janky quadtree optimization

--- src/components/client/views/graphics/marching_squares.ts
+++ src/components/client/views/graphics/marching_squares.ts
@@ -7,57 +7,34 @@ type RealCoord = {
     y: number,
 }
 
-type SquareCoord = {
-    xs: number,
-    ys: number,
-}
-
-const default_square_size = 16;
-
-function memoize(fn: ImplicitFunction): ImplicitFunction {
-    const memo: {[x: number]: {[y: number]: number}} = {};
-    return (x, y) => {
-        memo[x] ??= {};
-
-        if (memo[x][y]) {
-            return memo[x][y];
-        }
-
-        const v = fn(x, y);
-        memo[x][y] = v;
-
-        return v;
-    };
-}
-
-function square_to_real(square: SquareCoord, square_size: number): [number, number] {
-    return [square.xs * square_size, square.ys * square_size];
-}
+type QuadLeaf = {
+    x: number,
+    y: number,
+    size: number,
+};
 
 export function draw_implicit(fn: ImplicitFunction, threshold: number, ctx: p5) {
     ctx.push();
 
-    fn = memoize(fn);
-
-    const square_size = default_square_size;
-
     const center = {x: ctx.width / 2, y: ctx.height / 2};
     ctx.translate(center.x, center.y);
 
-    for_all_cells((real, square) => {
-        ctx.point(real.x, real.y);
+    const quadleaves = get_all_quadleaves(fn, threshold, ctx);
+
+    for (const leaf of quadleaves) {
+        ctx.point(leaf.x, leaf.y);
 
         const {
             top_left: tl,
             top_right: tr,
             bot_left: bl,
             bot_right: br,
-        } = get_corners(square);
+        } = get_corners(leaf);
 
-        const top_left_above = fn(...square_to_real(tl, square_size)) > threshold;
-        const top_right_above = fn(...square_to_real(tr, square_size)) > threshold;
-        const bot_right_above = fn(...square_to_real(br, square_size)) > threshold;
-        const bot_left_above = fn(...square_to_real(bl, square_size)) > threshold;
+        const top_left_above = fn(tl.x, tl.y) > threshold;
+        const top_right_above = fn(tr.x, tr.y) > threshold;
+        const bot_right_above = fn(br.x, br.y) > threshold;
+        const bot_left_above = fn(bl.x, bl.y) > threshold;
 
         const pattern =
             (top_left_above ? 8 : 0) +
@@ -65,7 +42,7 @@ export function draw_implicit(fn: ImplicitFunction, threshold: number, ctx: p5) 
             (bot_right_above ? 2 : 0) +
             (bot_left_above ? 1 : 0);
 
-        const point_groups: Array<[SquareCoord, SquareCoord]> =
+        const point_groups: Array<[RealCoord, RealCoord]> =
             pattern === 1 ? [[
                 get_isoline_point_between(bl, br, "x"),
                 get_isoline_point_between(bl, tl, "y"),
@@ -122,61 +99,107 @@ export function draw_implicit(fn: ImplicitFunction, threshold: number, ctx: p5) 
             ]] : [];
 
         for (const [a, b] of point_groups) {
-            const [ax, ay] = square_to_real(a, square_size);
-            const [bx, by] = square_to_real(b, square_size);
-            ctx.line(ax, ay, bx, by);
+            ctx.line(a.x, a.y, b.x, b.y);
         }
 
-        function get_isoline_point_between(a: SquareCoord, b: SquareCoord, orientation: "x" | "y"): SquareCoord {
-            const dimension: keyof SquareCoord = orientation === "x" ? "xs" : "ys";
-            const fixed_dimension: keyof SquareCoord = dimension === "xs" ? "ys" : "xs";
+        function get_isoline_point_between(a: RealCoord, b: RealCoord, orientation: "x" | "y"): RealCoord {
+            const dimension = orientation;
+            const fixed_dimension = dimension === "x" ? "y" : "x";
 
             const point_var = ctx.map(
                 threshold,
-                fn(...square_to_real(a, square_size)), fn(...square_to_real(b, square_size)),
+                fn(a.x, a.y), fn(b.x, b.y),
                 a[dimension], b[dimension]
             );
 
             const point = {
                 [dimension]: point_var,
                 [fixed_dimension]: a[fixed_dimension],
-            } as SquareCoord;
+            } as RealCoord;
 
             return point;
         }
-    }, square_size, ctx);
+    }
 
     ctx.pop();
 }
 
-function draw_shape(points: Array<SquareCoord>, square_size: number, ctx: p5) {
-    ctx.beginShape();
+function get_all_quadleaves(fn: ImplicitFunction, threshold: number, ctx: p5): Array<QuadLeaf> {
+    const x_span = Math.ceil(ctx.width / 2);
+    const y_span = Math.ceil(ctx.height / 2);
 
-    for (const point of points) {
-        ctx.vertex(...square_to_real(point, square_size));
+    const min_span = ctx.min(x_span, y_span);
+    const min_size = ctx.min(ctx.width, ctx.height);
+
+    function split(leaf: QuadLeaf): [QuadLeaf, QuadLeaf, QuadLeaf, QuadLeaf] {
+        const split_size = leaf.size / 2;
+
+        const top_left = {x: leaf.x, y: leaf.y, size: split_size};
+        const top_right = {x: leaf.x + split_size, y: leaf.y, size: split_size};
+        const bot_left = {x: leaf.x, y: leaf.y + split_size, size: split_size};
+        const bot_right = {x: leaf.x + split_size, y: leaf.y + split_size, size: split_size};
+
+        return [top_left, top_right, bot_left, bot_right];
     }
 
-    ctx.endShape();
-}
+    const root: QuadLeaf = {
+        x: -min_span,
+        y: -min_span,
+        size: min_size,
+    };
 
-function for_all_cells(fn: (real: RealCoord, square: SquareCoord) => void, square_size: number, ctx: p5) {
-    const y_span = Math.ceil(ctx.height / 2 / square_size) * square_size;
-    const x_span = Math.ceil(ctx.width / 2 / square_size) * square_size;
+    let leaves = [root];
 
-    for (let y = -y_span; y < y_span; y += square_size) {
-        for (let x = -x_span; x < x_span; x += square_size) {
-            const ys = y / square_size;
-            const xs = x / square_size;
+    const initial_splits = 4;
+    for (let i = 0; i < initial_splits; i++) {
+        leaves = leaves.flatMap(split);
+    }
 
-            fn({x, y}, {xs, ys});
+    function is_leaf_interesting(leaf: QuadLeaf): boolean {
+        const top_left_above = fn(leaf.x, leaf.y) > threshold;
+        const top_right_above = fn(leaf.x + leaf.size, leaf.y) > threshold;
+        const bot_left_above = fn(leaf.x, leaf.y + leaf.size) > threshold;
+        const bot_right_above = fn(leaf.x + leaf.size, leaf.y + leaf.size) > threshold;
+        const center_above = fn(leaf.x + leaf.size / 2, leaf.y + leaf.size / 2) > threshold;
+
+        return !(
+            top_left_above === top_right_above &&
+            top_right_above === bot_left_above &&
+            bot_left_above === bot_right_above &&
+            bot_right_above === center_above
+        );
+    }
+
+    const max_deep_splits = 4;
+    leaves = leaves.flatMap(leaf => {
+        function split_recurse(leaf: QuadLeaf, depth: number): Array<QuadLeaf> {
+            if (depth >= max_deep_splits) {
+                return [leaf];
+            }
+
+            if (is_leaf_interesting(leaf)) {
+                return split(leaf).flatMap(child => split_recurse(child, depth + 1));
+            }
+
+            return [leaf];
         }
-    }
+
+        return split_recurse(leaf, 1);
+    })
+
+    return leaves;
 }
 
-function get_corners(top_left: SquareCoord) {
-    const top_right = {xs: top_left.xs + 1, ys: top_left.ys    };
-    const bot_left  = {xs: top_left.xs    , ys: top_left.ys + 1};
-    const bot_right = {xs: top_left.xs + 1, ys: top_left.ys + 1};
+function get_corners(leaf: QuadLeaf): {
+    top_left: RealCoord,
+    top_right: RealCoord,
+    bot_left: RealCoord,
+    bot_right: RealCoord,
+} {
+    const top_left = {x: leaf.x, y: leaf.y};
+    const top_right = {x: leaf.x + leaf.size, y: leaf.y};
+    const bot_left  = {x: leaf.x, y: leaf.y + leaf.size};
+    const bot_right = {x: leaf.x + leaf.size, y: leaf.y + leaf.size};
 
     return {top_left, top_right, bot_left, bot_right};
 }
