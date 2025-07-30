9d1e06de466b65a15180942607c129b9acbc5bff
pierce-smith1 <pierce.smith@rezstream.com>
2025-07-29 22:02:31 -0600

    (api) (docs) (#16) Refine splash API docs

--- src/api/splashes.ts
+++ src/api/splashes.ts
@@ -52,7 +52,8 @@ export const paged_splash_request_endpoint = {
 
 export const discord_splash_upload_endpoint = {
     method: "POST",
-    description: "Submit a splash that was created by a Discord user into the splash queue. Only one splash for a particular user can be submitter per 24 hours; if more than one is attempted, the request will fail with a 400.",
+    protected: true,
+    description: "Submit a splash that was created by a Discord user into the splash queue. Only one splash for a particular user can be submitted per 24 hours; if more than one is attempted, the request will fail with a 400.",
     request: discord_splash_upload_request,
     response: {},
 } as const;
