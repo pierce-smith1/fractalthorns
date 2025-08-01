881685ce6d167b1368ec90e297fad83bca777a60
pierce-smith1 <pierce.smith@rezstream.com>
2025-07-31 21:09:53 -0600

    (api) (client) Integrate splashes with home page + fix some bugs

--- src/config.ts
+++ src/config.ts
@@ -8,15 +8,18 @@ function get_or_die(key: string): string {
     return value;
 }
 
-function get_or_default(key: string, _default: string) {
+function get_or_default(key: string, default_: string) {
     const value = process.env[key];
-    return value ?? _default;
+    return value ?? default_;
 }
 
 const config = {
     self_url: get_or_die("RVA_SELF_URL"),
     content_root: get_or_default("RVA_CONTENT_ROOT", "./_content"),
     database_url: get_or_die("DATABASE_URL"),
+
+    splash_rate_limit_ms: get_or_default("RVA_SPLASH_RATE_LIMIT_MS", `${1000 * 60 * 60 * 24}`),
+    splash_advance_interval_ms: get_or_default("RVA_SPLASH_ADVANCE_INTERVAL", `${1000 * 60 * 60 * 24}`),
 
     env: get_or_default("RVA_ENV", "prod"),
 };
